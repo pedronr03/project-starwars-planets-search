71913ad8f46b2acdb0d736b9a5e934d209640876
pedronr03 <pedronr0309@gmail.com>
2022-06-13 08:11:48 -0300

    Requisito 3 e 4.

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,24 +1,99 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PlanetsContext from '../../context/PlanetsContext';
 import Table from '../Table/Table';
+
+const INITIAL_STATE = {
+  column: 'population',
+  comparison: 'maior que',
+  value: 0,
+};
+
+const columnFilter = [
+  'population',
+  'orbital_period',
+  'diameter',
+  'rotation_period',
+  'surface_water',
+];
+
+const comparisonFilter = [
+  'maior que',
+  'menor que',
+  'igual a',
+];
 
 export default function MainPage() {
   const {
     onChangeInput,
+    setNewFilter,
     filter: { filterByName: { name } },
   } = useContext(PlanetsContext);
+  const [filter, setFilter] = useState(INITIAL_STATE);
+
+  const handle = ({ target }) => setFilter({ ...filter, [target.id]: target.value });
+
+  const getFilter = () => {
+    setNewFilter({ ...filter });
+    setFilter(INITIAL_STATE);
+  };
 
   return (
     <div>
-      <label htmlFor="inputFilter">
-        <input
-          id="inputFilter"
-          type="text"
-          value={ name }
-          data-testid="name-filter"
-          onChange={ onChangeInput }
-        />
-      </label>
+      <form>
+        <label htmlFor="inputFilter">
+          <input
+            id="inputFilter"
+            type="text"
+            value={ name }
+            data-testid="name-filter"
+            onChange={ onChangeInput }
+          />
+        </label>
+        <label htmlFor="columnFilter">
+          <select
+            id="column"
+            data-testid="column-filter"
+            value={ filter.column }
+            onChange={ handle }
+          >
+            {
+              columnFilter.map((column) => (
+                <option key={ column } value={ column }>{ column }</option>
+              ))
+            }
+          </select>
+        </label>
+        <label htmlFor="comparison">
+          <select
+            id="comparison"
+            data-testid="comparison-filter"
+            value={ filter.comparison }
+            onChange={ handle }
+          >
+            {
+              comparisonFilter.map((comparison) => (
+                <option key={ comparison } value={ comparison }>{ comparison }</option>
+              ))
+            }
+          </select>
+        </label>
+        <label htmlFor="value">
+          <input
+            value={ filter.value }
+            type="number"
+            id="value"
+            data-testid="value-filter"
+            onChange={ handle }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ getFilter }
+        >
+          Filtrar
+        </button>
+      </form>
       <Table />
     </div>
   );
--- src/context/PlanetsProvider.jsx
+++ src/context/PlanetsProvider.jsx
@@ -8,23 +8,52 @@ export default function PlanetsContext({ children }) {
   const [request, setRequest] = useState([]);
   const [filter, setFilter] = useState({
     filterByName: { name: '' },
+    filterByNumericValues: [],
   });
 
   const onChangeInput = ({ target: { value } }) => {
     setFilter({ ...filter, filterByName: { name: value } });
   };
 
+  const setNewFilter = (newFilter) => {
+    setFilter((prev) => ({
+      ...filter,
+      filterByNumericValues: prev.filterByNumericValues.concat(newFilter),
+    }));
+  };
+
   useEffect(() => {
     const filterByName = () => {
       if (!requestAPI.length) return;
       const { name } = filter.filterByName;
-      const map = requestAPI.filter((planet) => planet.name.includes(name));
+      const map = requestAPI
+        .filter((planet) => planet.name.toLowerCase().includes(name.toLowerCase()));
       setRequest(map);
     };
-    filterByName();
-  }, [filter.filterByName.name, requestAPI]);
 
-  const context = { request, onChangeInput, filter };
+    const filterByNumeric = () => {
+      const { filterByNumericValues } = filter;
+      if (!filterByNumericValues.length) return;
+      let newList = [...request];
+      filterByNumericValues.forEach((filterObj) => {
+        newList = newList.filter((planet) => {
+          if (filterObj.comparison === 'maior que') {
+            return Number(planet[filterObj.column]) > filterObj.value;
+          }
+          if (filterObj.comparison === 'menor que') {
+            return Number(planet[filterObj.column]) < filterObj.value;
+          }
+          return planet[filterObj.column] === filterObj.value;
+        });
+      });
+      setRequest(newList);
+    };
+
+    filterByName();
+    filterByNumeric();
+  }, [filter.filterByName, requestAPI, filter.filterByNumericValues]);
+
+  const context = { request, onChangeInput, filter, setNewFilter };
 
   return (
     <Context.Provider value={ context }>
